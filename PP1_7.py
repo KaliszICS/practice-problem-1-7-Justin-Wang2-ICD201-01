0a7649a29f5d85c320aaa508ce322936afdb6ec0
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-05 10:20:28 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,20 +1,21 @@
 
 
 q1():
-ToF = True
-print(ToF)
+true = True
+print(true)
 q2():
   num = input("Input an integer: ")
+  num1 = 5
   num = int(num)
-  bool = num > 5
+  bool = num > num1
   print(bool)
 q3():
   num = input("Input the letter a:")
-  bool = num == "a"
+  bool = (num == "a")
   print(bool)
 q4():
   word = input( "Input a word earlier in the dictionary than google: ")
-  bool = word < "google"
+  bool = (word < "google")
   print(bool)
 
 q5():
@@ -23,9 +24,8 @@ q5():
   word = int(word)
   word1 = int(word1)
   word2 = word * word1
-  word2 = int(word2)
   boole = word2 > 40
-  print(f"Your numbers multiplied together are greater than 40: {boole}")
+  print(f"Your numbers multiplied together are greater than 40:", {boole})
 
 
 #Do edit the code below
