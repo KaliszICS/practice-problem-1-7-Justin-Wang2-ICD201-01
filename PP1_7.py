f53a725a01f8ff496be9f36af021e7c65cc4b1a4
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-05 14:19:03 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,31 +1,34 @@
 
 
 q1():
-  true = True
+  true=True
   print(true)
+
 q2():
-  num = input("Input an integer: ")
-  num1 = 5
-  num1 = int(num1)
-  num = int(num)
-  bool = num1 < num
+  num=input("Input an integer: ")
+  num1= 5
+  num1= int(num1)
+  num= int(num)
+  bool= num1 < num
   print(bool)
+
 q3():
-  num = input("Input the letter a:")
-  bool = (num == "a")
+  word=input("Input the letter a: ")
+  bool= (word =="a")
   print(bool)
+
 q4():
-  word = input( "Input a word earlier in the dictionary than google: ")
-  bool = (word < "google")
+  word=input("Input a word earlier in the dictionary than google: ")
+  bool= (word<"google")
   print(bool)
+
 
 q5():
-  word = int(input("Input an integer: "))
-  word1 = int(input("Input another integer: "))
-  word2 = word * word1
-  boole = word2 > 40
-  print("Your numbers multiplied together are greater than 40:", boole)
-
+  num = int(input("Input an integer: "))
+  num1 = int(input("Input another integer: "))
+  num2 = num * num1
+  end = num2 > 40
+  print("Your numbers multiplied together are greater than 40:", end)
 
 #Do edit the code below
 #Comment the lines below when running your tests
